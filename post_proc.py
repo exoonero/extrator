ed71b2657ca63a6ca209357cbaec830b9a678ee2
Alex Custódio <89322317+alex-custodio@users.noreply.github.com>
2022-09-14 14:06:43 -0300

    Update post_proc.py
    Co-authored-by: Daniel Fireman <danielfireman@gmail.com>

--- post_proc.py
+++ post_proc.py
@@ -1,7 +1,7 @@
 # Pra remover os espaços em branco no início, lstrip
 
 in_file = open("diario-anadia-2022-08-29-extraido.txt", "r")
-out_file_text = in_file.read().lstrip()
+out_text = in_file.read().lstrip()
 out_file = open("diario-anadia-2022-08-29-proc.txt", "w")
 out_file.write(out_text)
 out_file.close()
