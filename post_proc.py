a9a99413256e5c1bd2140777429de9b64781bdd0
Alex Custódio <89322317+alex-custodio@users.noreply.github.com>
2022-09-14 14:06:33 -0300

    Update post_proc.py
    Co-authored-by: Daniel Fireman <danielfireman@gmail.com>

--- post_proc.py
+++ post_proc.py
@@ -3,6 +3,6 @@
 in_file = open("diario-anadia-2022-08-29-extraido.txt", "r")
 out_file_text = in_file.read().lstrip()
 out_file = open("diario-anadia-2022-08-29-proc.txt", "w")
-out_file.write(out_file_text)
+out_file.write(out_text)
 out_file.close()
     
