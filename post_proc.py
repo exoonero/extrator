5dc22773e5a89d1d41e2fadd7e85403db2a2054c
Alex Custódio <alex.scustod@gmail.com>
2022-09-14 14:11:44 +0000

    post_proc modificado com remoção das linhas

--- post_proc.py
+++ post_proc.py
@@ -1,8 +1,8 @@
-from bs4 import BeautifulSoup
+# Pra remover os espaços em branco, lstript
 
 in_file = open("diario-anadia-2022-08-29-extraido.txt", "r")
-html = BeautifulSoup(in_file.read(), "html.parser")  
-
+out_file_text = in_file.read().lstrip()
 out_file = open("diario-anadia-2022-08-29-proc.txt", "w")
-out_file.write(html.get_text())
+out_file.write(out_file_text)
 out_file.close()
+    
