bd0357e478d83232c5329a1adbee201cdbdea6e0
Alex Custódio <alex.scustod@gmail.com>
2022-09-15 23:43:33 +0000

    separando em funções

--- post_proc.py
+++ post_proc.py
@@ -3,17 +3,24 @@ in_file = open("diario-anadia-2022-08-29-extraido.txt", "r")
 in_text = in_file.read().lstrip()
 in_text = in_text.splitlines()
 ama_header = in_text[0]
+def generate_text():
+    final_text = ""
+    for linha in range(len(in_text)):
+        final_text += in_text[linha] + "\n"
+    return final_text
 #alterar as outras ocorrências do header do ama
-for linha in range(len(in_text)):
-    if linha == 0:
-        continue
-    if in_text[linha] == ama_header:
-        in_text[linha] = in_text[linha].replace(in_text[linha], "")
-#formar o novo texto
-final_text = ""
-for linha in range(len(in_text)):
-    final_text += in_text[linha] + "\n"
-print(final_text)
-out_file = open("diario-anadia-2022-08-29-proc.txt", "w")
-out_file.write(final_text)
-out_file.close()
+def delete_header():
+    for linha in range(len(in_text)):
+        if linha == 0:
+            continue
+        if in_text[linha] == ama_header:
+            in_text[linha] = in_text[linha].replace(in_text[linha], "")
+def create_file(final_text):  
+    out_file = open("diario-anadia-2022-08-29-proc.txt", "w")
+    out_file.write(final_text)
+    out_file.close()
+
+#chamadas
+delete_header()
+create_file(generate_text())
+
