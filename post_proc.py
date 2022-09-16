8721ecb7380e0891a8c312a02fc0392fce96839a
Alex Custódio <alex.scustod@gmail.com>
2022-09-16 02:37:44 +0000

    fazendo uma comparação melhor de cabeçalho

--- post_proc.py
+++ post_proc.py
@@ -2,7 +2,8 @@ in_file = open("diario-anadia-2022-08-29-extraido.txt", "r")
 #Mudei o nome da variável out_text para in_text, pois ela não será mais usada como texto de saída.
 in_text = in_file.read().lstrip()
 in_text = in_text.splitlines()
-ama_header = in_text[0]
+ama_header_words = in_text[0].split()
+ama_header_id = ama_header_words[0]
 def generate_text():
     final_text = ""
     for linha in range(len(in_text)):
@@ -13,7 +14,7 @@ def delete_header():
     for linha in range(len(in_text)):
         if linha == 0:
             continue
-        if in_text[linha] == ama_header:
+        if in_text[linha].startswith(ama_header_id):
             in_text[linha] = in_text[linha].replace(in_text[linha], "")
 def create_file(final_text):  
     out_file = open("diario-anadia-2022-08-29-proc.txt", "w")
