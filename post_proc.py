9646a7c074f08dcd311fa6599548d8dca8e90cf3
Alex Custódio <alex.scustod@gmail.com>
2022-09-20 18:41:16 +0000

    Formatação adicionada

--- post_proc.py
+++ post_proc.py
@@ -1,26 +1,33 @@
 in_file = open("diario-anadia-2022-08-29-extraido.txt", "r")
-#Mudei o nome da variável out_text para in_text, pois ela não será mais usada como texto de saída.
+# Mudei o nome da variável out_text para in_text, pois ela não será mais usada como texto de saída.
 in_text = in_file.read().lstrip()
 in_text = in_text.splitlines()
 ama_header_words = in_text[0].split()
 ama_header_id = ama_header_words[0]
+
+
 def generate_text():
     final_text = ""
     for linha in range(len(in_text)):
         final_text += in_text[linha] + "\n"
     return final_text
-#alterar as outras ocorrências do header do ama
+# alterar as outras ocorrências do header do ama
+
+
 def delete_header():
     for linha in range(len(in_text)):
         if linha == 0:
             continue
         if in_text[linha].startswith(ama_header_id):
             in_text[linha] = in_text[linha].replace(in_text[linha], "")
-def create_file(final_text):  
+
+
+def create_file(final_text):
     out_file = open("diario-anadia-2022-08-29-proc.txt", "w")
     out_file.write(final_text)
     out_file.close()
-#chamadas
+
+
+# chamadas
 delete_header()
 create_file(generate_text())
-
