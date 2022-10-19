e98ba09a5b6db3244d68a247cf0e83563c89f6fd
Alex Custódio <alex.scustod@gmail.com>
2022-10-19 18:51:42 +0000

    corrigindo diretório do texto extraído

--- post_proc_test.py
+++ post_proc_test.py
@@ -6,7 +6,7 @@ class PostProcTest(unittest.TestCase):
 
     def test_qtd_municipios_2022_08_29(self):
         self.assertEqual(33, len(extrai_diarios(
-            'diario-completo-2022-08-29/diario-completo-2022-08-29-extraido.txt')))
+            'test_data/diario-completo-2022-08-29-extraido.txt')))
 
 
 if __name__ == '__main__':
