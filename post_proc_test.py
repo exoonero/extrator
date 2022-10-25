05a620a701c115a097c13413febe237ab16fbe0f
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2022-10-25 18:07:51 +0000

    adicionando-testes

--- post_proc_test.py
+++ post_proc_test.py
@@ -10,8 +10,9 @@ class PostProcTest(unittest.TestCase):
         # Novos casos de teste devem ser adicionados na tupla abaixo.
         # Deve ser adicionado um arquivo -test.json para cada texto extraído (caso de teste)
         cases = (
+            'test_data/diario-completo-2022-01-03-test.json',
             'test_data/diario-completo-2022-08-29-test.json',
-            'test_data/diario-completo-2022-07-20-test.json',
+            'test_data/diario-completo-2022-07-20-test.json'
         )
 
         for case_path in cases:
