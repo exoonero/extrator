69751ace5d13cdf201d36bdc30ffe31c23ccc3b4
Alex Custódio <alex.scustod@gmail.com>
2022-12-19 02:46:40 +0000

    testando test

--- post_proc_test.py
+++ post_proc_test.py
@@ -22,7 +22,7 @@ class PostProcTest(unittest.TestCase):
             # 'test_data/diario-completo-2020-10-01-test.json',
             'test_data/diario-completo-2020-06-29-test.json',
             'test_data/diario-completo-2020-05-14-test.json',
-            # 'test_data/diario-completo-2019-08-14-test.json',
+            'test_data/diario-completo-2019-08-14-test.json',
             # 'test_data/diario-completo-2019-07-02-test.json',
             'test_data/diario-completo-2018-09-28-test.json',
             # 'test_data/diario-completo-2018-10-02-test.json',
