bf6298b8440c11d8304eae399f95462aff57b63d
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2023-01-10 00:20:13 +0000

    preenchendo o json com informações do diario

--- post_proc_test.py
+++ post_proc_test.py
@@ -10,6 +10,7 @@ class PostProcTest(unittest.TestCase):
         # Novos casos de teste devem ser adicionados na tupla abaixo.
         # Deve ser adicionado um arquivo -test.json para cada texto extraído (caso de teste)
         cases = (
+            'test_data/diario-completo-2023-01-02-test.json',
             'test_data/diario-completo-2022-01-03-test.json',
             'test_data/diario-completo-2022-08-15-test.json',
             'test_data/diario-completo-2022-08-29-test.json',
