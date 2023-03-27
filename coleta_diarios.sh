f88caf86d32de25dfab8bbe2e133d493858ffdc1
Daniel Fireman <danielfireman@gmail.com>
2023-03-27 18:34:27 -0300

    Update coleta_diarios.sh

--- coleta_diarios.sh
+++ coleta_diarios.sh
@@ -21,39 +21,39 @@ mkdir -p ${OUT_DIR}
 # Checando se o docker está rodando antes de iniciar a coleta.
 docker ps > /dev/null
 
-# # Preparando ambiente para coleta.
-# cd ${REPO_DIR} || (git clone https://github.com/okfn-brasil/querido-diario qd && cd ${REPO_DIR})
-# python3 -m venv .venv
-# source .venv/bin/activate
-# pip install -r ${DATA_COLLECTION_DIR}/requirements-dev.txt
-# pre-commit install
+# Preparando ambiente para coleta.
+cd ${REPO_DIR} || (git clone https://github.com/okfn-brasil/querido-diario qd && cd ${REPO_DIR})
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r ${DATA_COLLECTION_DIR}/requirements-dev.txt
+pre-commit install
 
-# # Coletando diários e movendo para a pasta diários.
-# cd ${DATA_COLLECTION_DIR}
-# scrapy crawl al_associacao_municipios -a start_date=${START_DATE} -a end_date=${END_DATE} > ${OUT_DIR}/scrapy.out 2> ${OUT_DIR}/scrapy.err
-# for dir in `ls -da ${QD_DOWNLOAD_DIR}/*`
-# do
-#     # Importante pois algumas datas possuem mais de um diário (tem os extras).
-#     i=1
-#     for fpath in `ls -da ${dir}/*`
-#     do
-#         newname=`basename ${dir}-${i}.pdf`
-#         mv ${fpath} ${DOWNLOAD_DIR}/${newname}
-#         i=$((i+1))
-#     done
-# done
+# Coletando diários e movendo para a pasta diários.
+cd ${DATA_COLLECTION_DIR}
+scrapy crawl al_associacao_municipios -a start_date=${START_DATE} -a end_date=${END_DATE} > ${OUT_DIR}/scrapy.out 2> ${OUT_DIR}/scrapy.err
+for dir in `ls -da ${QD_DOWNLOAD_DIR}/*`
+do
+    # Importante pois algumas datas possuem mais de um diário (tem os extras).
+    i=1
+    for fpath in `ls -da ${dir}/*`
+    do
+        newname=`basename ${dir}-${i}.pdf`
+        mv ${fpath} ${DOWNLOAD_DIR}/${newname}
+        i=$((i+1))
+    done
+done
 
-# # Finalizando e saindo do ambiente virtual.
-# cd ${REPO_DIR}
-# pre-commit uninstall
-# deactivate
+# Finalizando e saindo do ambiente virtual.
+cd ${REPO_DIR}
+pre-commit uninstall
+deactivate
 
 # Extraindo texto dos diários e segmentando diários.
 cd ${DOWNLOAD_DIR}
 
 # docker pull apache/tika:1.28.4
-# docker run -d -p 9998:9998 --rm --name tika apache/tika:1.28.4
-# sleep 10
+docker run -d -p 9998:9998 --rm --name tika apache/tika:1.28.4
+sleep 10
 
 for pdf in `ls -a *.pdf`
 do
