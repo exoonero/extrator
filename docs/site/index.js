d225b73de26ad25dc656cce70bce055dd8e050f4
Daniel Fireman <danielfireman@gmail.com>
2023-04-10 21:13:41 -0300

    Revamp site

--- docs/site/index.js
+++ docs/site/index.js
@@ -2,26 +2,18 @@ const selectElement = document.querySelector('#municipio-select');
 
 selectElement.addEventListener('change', updateData);
 
-function createId(nome) {
-    var id = nome.trim().toLowerCase().replace(" ", "").normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-    return id
-}
-
 function updateData() {
     var municipio = selectElement.value;
-    municipio = createId(municipio)
     const cards = document.querySelectorAll('.card');
     cards.forEach(card => {
         const ano = card.id;
-        fetch(`./diarios/${ano.split('-')[1]}/${municipio}-inicial.json`)
+        fetch(`./dados/inicial/${municipio}-inicial.json`)
             .then(response => response.json())
             .then(data => {
-                const mediaDiariosMesValueElement = document.querySelector(`#${ano} #media-diarios-mes`);
-                const totalDiariosAnoValueElement = document.querySelector(`#${ano} #total-diarios-ano`);
-                const mediaDiariosMes = data.resumo.media_diarios_mes;
-                const totalDiariosAno = data.resumo.total_diarios_ano;
-                mediaDiariosMesValueElement.textContent = mediaDiariosMes.toFixed(1);
-                totalDiariosAnoValueElement.textContent = totalDiariosAno;
+                const numDiariosAnoValueElement = document.querySelector(`#${ano} #num-diarios`);
+                const detalhe_ano = data.detalhe[ano]
+                const numDiariosAno = detalhe_ano.num_diarios;
+                numDiariosAnoValueElement.textContent = numDiariosAno;
             });
     });
 }
--- docs/site/site_home_data.py
+++ docs/site/site_home_data.py
@@ -2,49 +2,74 @@ import json
 import glob
 import os
 
-contagem_diarios = {}
-municipios = {}
-for path in glob.glob("data/diarios/*-resumo-extracao.json"):
-    with open(path, encoding="utf-8") as json_file:
+os.makedirs("./docs/site/dados/inicial", exist_ok=True)
+
+inicial = {}  # Dicionário que guarda dados para renderização da página inicial.
+geral = {
+    "detalhe": {},
+}  
+for path in glob.glob("./data/diarios/*-atos.json"):
+    with open(path) as json_file:
         diarios = json.load(json_file)
+
         for diario in diarios:
             diario = json.loads(diario)
-            data_str = os.path.basename(path).split('-resumo')[0]
-            ano, mes = map(int, data_str.split("-")[:2])
-            id = diario["id"]
-            municipios[id] = diario["municipio"]
-            if id not in contagem_diarios:
-                contagem_diarios[id] = {}
-            if (ano, mes) not in contagem_diarios[id]:
-                contagem_diarios[id][(ano, mes)] = 0
-            contagem_diarios[id][(ano, mes)] += 1
+            id_municipio = diario["id"]
+            nome_municipio = diario["municipio"]
 
-diarios_por_id = {}
-for id, contagem in contagem_diarios.items():
-    total_ano = sum(contagem.values())
-    media_diarios_mes = total_ano / len(contagem)
-    info_total = {
-        "id": id,
-        "nome": municipios[id],
-        "resumo": {
-            "total_diarios_ano": total_ano,
-            "media_diarios_mes": media_diarios_mes
-        },
-        "detalhe": [
-            {
-                'ano': ano,
-                'mes': mes,
-                'diarios': {'qtd-diarios': qtd},
-                'atos': {},
-                'nomeacoes': {},
-                'exoneracoes': {}
-            } for (ano, mes), qtd in contagem.items()
-        ]
+            data_quebrada = diario["data_publicacao"].split("-")
+            ano = int(data_quebrada[0])
+            mes = int(data_quebrada[1])  # para uso futuro
+            dia = int(data_quebrada[2])  # para uso futuro
+
+            # Atualizando seção de detalhes do municipio
+            dado_municipio = inicial.get(id_municipio, {})
+            detalhe = dado_municipio.get("detalhe", {})
+            detalhe_ano = detalhe.get(ano, {})
+            detalhe_ano["num_diarios"] = detalhe_ano.get("num_diarios", 0) + 1 
+            for ato in diario["atos"]:
+                ato = json.loads(ato)
+                detalhe_ano["num_nomeacoes"] = detalhe_ano.get("num_nomeacoes", 0) + len(ato["cpf_nomeacoes"])
+                detalhe_ano["num_exoneracoes"] = detalhe_ano.get("num_exoneracoes", 0) + len(ato["cpf_exoneracoes"])
+            detalhe[ano] = detalhe_ano
+
+            inicial[id_municipio] = {
+                "id": id_municipio,
+                "nome": nome_municipio,
+                "detalhe": detalhe,
+            }
+
+            # Atualizando seção de detalhes geral.
+            detalhe_geral = geral.get("detalhe", {})
+            detalhe_geral_ano = detalhe_geral.get(ano, {})
+            detalhe_geral_ano["num_diarios"] = detalhe_geral_ano.get("num_diarios", 0) + 1
+            detalhe_geral_ano["num_nomeacoes"] = detalhe_geral_ano.get("num_nomeacoes", 0) + detalhe_ano.get("num_nomeacoes", 0)
+            detalhe_geral_ano["num_exoneracoes"] = detalhe_geral_ano.get("num_exoneracoes", 0) + detalhe_ano.get("num_exoneracoes", 0)
+            detalhe_geral[ano] = detalhe_geral_ano
+
+            inicial["geral"] = {
+                "id": "geral",
+                "detalhe": detalhe_geral,
+            }
+
+# Atualizando seção de resumo
+for id_municipio, dado in inicial.items():
+    num_diarios = 0
+    num_exoneracoes = 0
+    num_nomeacoes = 0
+
+    for ano, detalhe in dado["detalhe"].items():
+        num_diarios += detalhe["num_diarios"]
+        num_exoneracoes += detalhe.get("num_exoneracoes", 0)
+        num_nomeacoes += detalhe.get("num_nomeacoes", 0)
+
+    inicial[id_municipio]["resumo"] = {
+        "num_diarios": num_diarios,
+        "num_exoneracoes": num_exoneracoes,
+        "num_nomeacoes": num_nomeacoes,    
     }
-    diarios_por_id[id] = info_total
 
-os.makedirs('./docs/site/diarios/2022', exist_ok=True)
-
-for (id, diarios) in diarios_por_id.items():
-    with open(f"./docs/site/diarios/2022/{id}-inicial.json", "w", encoding="utf-8") as json_file:
-        json.dump(diarios, json_file, indent=2, default=str, ensure_ascii=False)
+# Salvando dados para renderização da página inicial.
+for id_municipio, dado in inicial.items():
+    with open(f"./docs/site/dados/inicial/{id_municipio}-inicial.json", "w", encoding="utf-8") as json_file:
+        json.dump(dado, json_file, indent=2, default=str, ensure_ascii=False)
