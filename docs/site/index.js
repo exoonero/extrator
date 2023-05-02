3b4a9a6bf7618e4bd4bafbe20575a6819a08a95f
Alex Custódio <alex.scustod@gmail.com>
2023-05-02 21:21:58 +0000

    adicionando primeiros testes com apex charts

--- docs/site/index.js
+++ docs/site/index.js
@@ -27,3 +27,20 @@ function updateData() {
   });
 }
 updateData();
+
+var options = {
+  chart: {
+    type: 'bar'
+  },
+  series: [{
+    name: 'sales',
+    data: [30,40,35,50,49,60,70,91,125]
+  }],
+  xaxis: {
+    categories: [2014,2015,2016,2017,2018,2019,2020,2021,2022,2023]
+  }
+}
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+chart.render();
