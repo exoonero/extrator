7fb4b32184550af3d1cd054319cf96e16c5ff323
Alex Custódio <alex.scustod@gmail.com>
2023-06-12 02:37:44 +0000

    gerando apenas um arquivo por municipio

--- docs/site_home_data.py
+++ docs/site_home_data.py
@@ -2,8 +2,7 @@ import json
 import glob
 import os
 
-os.makedirs("./docs/site/dados/inicial", exist_ok=True)
-os.makedirs("./docs/site/dados/municipio", exist_ok=True)
+os.makedirs("./docs/site/dados", exist_ok=True)
 
 # Dicionário que guarda dados para renderização da página inicial.
 inicial = {}
@@ -29,51 +28,21 @@ for path in glob.glob("./data/diarios/*-atos.json"):
             dado_municipio = inicial.get(id_municipio, {})
             detalhe = dado_municipio.get("detalhe", {})
             detalhe_ano = detalhe.get(ano, {})
-            detalhe_ano["num_diarios"] = detalhe_ano.get("num_diarios", 0) + 1
+            detalhe_ano_mes = detalhe_ano.get(mes, {})
+            detalhe_ano_mes["num_diarios"] = detalhe_ano_mes.get("num_diarios", 0) + 1
             for ato in diario["atos"]:
                 ato = json.loads(ato)
-                detalhe_ano["num_nomeacoes"] = detalhe_ano.get(
+                detalhe_ano_mes["num_nomeacoes"] = detalhe_ano_mes.get(
                     "num_nomeacoes", 0) + len(ato["cpf_nomeacoes"])
-                detalhe_ano["num_exoneracoes"] = detalhe_ano.get(
+                detalhe_ano_mes["num_exoneracoes"] = detalhe_ano_mes.get(
                     "num_exoneracoes", 0) + len(ato["cpf_exoneracoes"])
+            detalhe_ano[mes] = detalhe_ano_mes
             detalhe[ano] = detalhe_ano
 
             inicial[id_municipio] = {
                 "id": id_municipio,
                 "nome": nome_municipio,
                 "detalhe": detalhe,
-            }
-
-            dado_municipio_mes = municipio.get(id_municipio, {})
-            dado_municipio_mes[ano] = dado_municipio_mes.get(ano, {})
-            dado_municipio_mes[ano][mes] = dado_municipio_mes[ano].get(mes, {})
-            dado_municipio_mes[ano][mes]["num_diarios"] = dado_municipio_mes[ano][mes].get("num_diarios", 0) + 1
-            for ato in diario["atos"]:
-                ato = json.loads(ato)
-                dado_municipio_mes[ano][mes]["num_nomeacoes"] = dado_municipio_mes[ano][mes].get(
-                    "num_nomeacoes", 0) + len(ato["cpf_nomeacoes"])
-                dado_municipio_mes[ano][mes]["num_exoneracoes"] = dado_municipio_mes[ano][mes]["num_exoneracoes"] = dado_municipio_mes[ano][mes].get(
-                    "num_exoneracoes", 0) + len(ato["cpf_exoneracoes"])
-            municipio[id_municipio] = {
-                "id": id_municipio,
-                "nome": nome_municipio,
-                ano: dado_municipio_mes[ano]
-            }
-
-            # Atualizando seção de detalhes geral.
-            detalhe_geral = geral.get("detalhe", {})
-            detalhe_geral_ano = detalhe_geral.get(ano, {})
-            detalhe_geral_ano["num_diarios"] = detalhe_geral_ano.get(
-                "num_diarios", 0) + 1
-            detalhe_geral_ano["num_nomeacoes"] = detalhe_geral_ano.get(
-                "num_nomeacoes", 0) + detalhe_ano.get("num_nomeacoes", 0)
-            detalhe_geral_ano["num_exoneracoes"] = detalhe_geral_ano.get(
-                "num_exoneracoes", 0) + detalhe_ano.get("num_exoneracoes", 0)
-            detalhe_geral[ano] = detalhe_geral_ano
-
-            inicial["geral"] = {
-                "id": "geral",
-                "detalhe": detalhe_geral,
             }
 
 # Atualizando seção de resumo
@@ -83,9 +52,10 @@ for id_municipio, dado in inicial.items():
     num_nomeacoes = 0
 
     for ano, detalhe in dado["detalhe"].items():
-        num_diarios += detalhe["num_diarios"]
-        num_exoneracoes += detalhe.get("num_exoneracoes", 0)
-        num_nomeacoes += detalhe.get("num_nomeacoes", 0)
+        for mes, mes_detalhe in detalhe.items():
+            num_diarios += mes_detalhe["num_diarios"]
+            num_exoneracoes += mes_detalhe.get("num_exoneracoes", 0)
+            num_nomeacoes += mes_detalhe.get("num_nomeacoes", 0)
 
     inicial[id_municipio]["resumo"] = {
         "num_diarios": num_diarios,
@@ -95,10 +65,5 @@ for id_municipio, dado in inicial.items():
 
 # Salvando dados para renderização da página inicial.
 for id_municipio, dado in inicial.items():
-    with open(f"./docs/site/dados/inicial/{id_municipio}-inicial.json", "w", encoding="utf-8") as json_file:
-        json.dump(dado, json_file, indent=2, default=str, ensure_ascii=False)
-
-# Salvando dados para renderização da página do municipio.
-for id_municipio, dado in municipio.items():
-    with open(f"./docs/site/dados/municipio/{id_municipio}.json", "w", encoding="utf-8") as json_file:
+    with open(f"./docs/site/dados/{id_municipio}.json", "w", encoding="utf-8") as json_file:
         json.dump(dado, json_file, indent=2, default=str, ensure_ascii=False)
