ead918563b4b9b64cc074a02a4bdf66a21c9db81
Alex Custódio <alex.scustod@gmail.com>
2023-07-06 21:53:30 +0000

    adicionando caso de teste e correção

--- integracao_test.py
+++ integracao_test.py
@@ -28,6 +28,7 @@ class IntegracaoTest(unittest.TestCase):
             'test_data/diario-completo-2022-01-06-test.json',
             'test_data/diario-completo-2021-04-01-test.json',
             'test_data/diario-completo-2021-03-29-test.json',
+            'test_data/diario-completo-2021-01-29-test.json',
             'test_data/diario-completo-2021-01-15-test.json',
             'test_data/diario-completo-2020-12-16-test.json',
             'test_data/diario-completo-2020-10-30-test.json',
