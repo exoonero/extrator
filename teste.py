bc1c640d6f276abce1a5617cb28e948fd21887b6
Alex Custódio <alex.scustod@gmail.com>
2023-07-13 21:09:48 +0000

    removendo arquivo para testar lógica

--- teste.py
+++
@@ -1,9 +0,0 @@
-import re
-
-regex = r'(\d{3})-?(\d{3})-?(\d{3})-?(\d{2})'
-substitution = r'\1\2\3-\4'
-
-text = '456-495-567-34 234345334-53234123234-23'
-result = re.sub(regex, substitution, text)
-
-print(result)
